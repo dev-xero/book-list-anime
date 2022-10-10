4ac23f75f9c3d0e13b11010a135a537328bd9d16
CodeNinja-tech <elo48858@gmail.com>
2022-10-10 14:09:08 +0100

    Fixed Problem with import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 import { BookContextProvider } from './context/BookContext'
 
 export default function App() {
   return (
     <div className="App">
       <BookContextProvider>
-
+        <NavBar />
       </BookContextProvider>
     </div>
   )
