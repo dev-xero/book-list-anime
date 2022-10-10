672aaea6b486b44948ab30e453b2954ad201a553
CodeNinja-tech <elo48858@gmail.com>
2022-10-10 16:31:20 +0100

    Integrated Local Storage

--- src/context/BookContext.jsx
+++ src/context/BookContext.jsx
@@ -1,14 +1,24 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useEffect } from 'react'
 import { bookReducer } from '../reducers/book.reducer'
 
 export const BookContext = createContext()
 
 export const BookContextProvider = ({ children }) => {
-  const [books, dispatch] = useReducer(bookReducer, [])
+  const LOCAL_STORAGE_KEY = 'anime_to_watch'
 
-	return (
-		<BookContext.Provider value={{ books, dispatch }}>
-			{ children }
-		</BookContext.Provider>
-	)
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    return localStorage.getItem(LOCAL_STORAGE_KEY)
+      ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+      : []
+  })
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(books))
+  }, [books])
+
+  return (
+    <BookContext.Provider value={{ books, dispatch }}>
+      {children}
+    </BookContext.Provider>
+  )
 }
